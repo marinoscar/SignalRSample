9dbc8fc1a473ab12f75145cfe9219abe269ecb3b
Oscar Marin <oscar.marin@entegreat.com>
2013-08-03 00:25:43 -0600

    Initial commit

---
+++ WebUI/Chart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace WebUI
+{
+    public class Chart : Hub
+    {
+        public void SendData(double data)
+        {
+            Clients.All.sendData(data);
+        }
+    }
+}
